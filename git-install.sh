2609d4677c782ed9f752fc1120d06979a59e07cd
Andrew Druffner <andrew@nomstock.com>
2020-02-03 19:15:57 -0800

    updated install script to allow for branches and dist install

--- git-install.sh
+++ git-install.sh
@@ -1,49 +1,69 @@
 #!/usr/bin/env bash
 
+REPO_NAME=git-get
+TEMP_DIR="${TMPDIR:-/tmp}"
+TEMP_REPO_DIR=$(mktemp -d "${TEMP_DIR}"/$REPO_NAME.XXXXXXXXX)
 
-repo_name=git-get
-temp_dir="${TMPDIR:-/tmp}"
-temp_repo_dir=$(mktemp -d "${temp_dir}"/$repo_name.XXXXXXXXX)
+die() {
+    echo "$*" 1>&2
+    exit 1
+}
 
-die() { echo "$*" 1>&2 ; exit 1; }
-
-clone (){
-
-cd "${temp_repo_dir}"
-git clone --no-checkout https://github.com/ajdruff/${repo_name}.git || die "git-get: something went wrong trying to download installation files. Check your connection and try again"
-
-
-
-
-cd "${repo_name}"
-
+get_latest_tag() {
     # Get new tags from the remote
-git fetch --tags
+    git fetch --tags
 
-# Get the latest tag name
-local latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+    # Get the latest tag name
+    local latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 
-# Checkout the latest tag
-git checkout -b "${latest_tag}"
+    echo "$latest_tag"
+return 0
+}
+
+clone() {
+
+    local branch
+
+    cd "${TEMP_REPO_DIR}"
+    git clone --no-checkout https://github.com/ajdruff/${REPO_NAME}.git || die "git-get: something went wrong trying to download installation files. Check your connection and try again"
+
+    cd "${REPO_NAME}"
+
+    if [-z "${1}" ]; then
+        branch=$(get_latest_tag)
+    else
+        branch="${1}"
+    fi
+
+    # Checkout the latest tag
+    git checkout -b "${branch}"
+
+    cd -
+
+    return 0
 
 }
 install() {
 
-echo 'installing git-get...'
+    echo 'installing git-get...'
     local install_dir=$(dirname $(which git))
 
-    sudo install ./git-get "${install_dir}"/
-    sudo install ./parser/git-get-parser.sh "${install_dir}"/
+    if [ -d "$TEMP_REPO_DIR/dist" ]; then
+        sudo install "$TEMP_REPO_DIR/dist/git-get" "${install_dir}"/
+    else
 
+        sudo install "$TEMP_REPO_DIR/git-get" "${install_dir}"/
+        sudo install "$TEMP_REPO_DIR/parser/parser.sh" "${install_dir}"/
 
+    fi
+
+    return 0
 
 }
 
 clone
 install
 
-
-rm -rf "${temp_repo_dir}"/"${repo_name}"
+rm -rf "${TEMP_REPO_DIR}"/"${REPO_NAME}"
 
 (git get -h && git get -v && echo 'git get installed!') || die "git-get installation failed"
-
